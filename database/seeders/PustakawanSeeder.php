e673fd34c40a73eeae7b978807341d59dbdac571
Qigelia Hasbulloh <hasbullohqigelia@gmail.com>
2023-12-04 19:59:26 +0700

    Laravel breeze & spatie

---
+++ database/seeders/PustakawanSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class PustakawanSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = User::Create([
+            'name' => 'pustakawan',
+            'email' => 'pustakawan@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('password'),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        $user->assignRole('pustakawan');
+    }
+}
